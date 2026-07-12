7be4c6aeababee634f31c2ce07efef516cb1674b
AlvaroCoe <alvarodlcoelho@gmail.com>
2026-07-11 23:00:28 -0300

    alteração toastify

--- projeto-AOE/front-end-AOE/src/main.jsx
+++ projeto-AOE/front-end-AOE/src/main.jsx
@@ -10,10 +10,12 @@ import { ThemeProvider } from './context/ThemeContext'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
 
     <ToastContainer
-      position="top-right"
+      position="top-center"
       autoClose={3000}
       theme="light"
       pauseOnHover
